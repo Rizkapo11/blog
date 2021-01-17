5eea8142a072fc54f8cb36f528a15517fcdbdabc
rizka <rizkaputriocta11@gmail.com>
2021-01-17 13:56:46 +0700

    project blog

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import PostReducer from './PostReducer';
+import usersReducer from './usersReducer'
+
+export default combineReducers({
+    posts: PostReducer,
+    users: usersReducer
+});
